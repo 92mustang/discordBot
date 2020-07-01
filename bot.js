b6510267cdb0010e4eb48696dca40d3f404a58e5
92mustang <67708842+92mustang@users.noreply.github.com>
2020-07-02 00:41:40 +0530

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const Commando = require('discord.js-commando');
+
+const bot = new Commando.Client();
+
+bot.on('ready', function (evt) {
+    console.info('Logged in as ' + bot.user.tag);
+});
+
+bot.registry.registerGroup('random', 'Random');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+bot.on('messageReactionAdd', async (reaction, user) => {
+		// When we receive a reaction we check if the reaction is partial or not
+	if (reaction.partial) {
+		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.log('Something went wrong when fetching the message: ', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+	}
+	// Now the message has been cached and is fully available
+	//console.log(reaction.message.author + 's message ' + reaction.message.content + ' gained a reaction!');
+	if(reaction.message.channel.id == 653696571610890292 && reaction.emoji.name == '📌'
+		&& reaction.message.member.roles.has('493325071377629194')) {
+		console.log(reaction.message.member.roles.has('493325071377629194'));
+		reaction.message.pin();
+	}
+	
+	if(reaction.message.channel.id == 723962040036753448 && reaction.emoji.name == '📌'
+		&& reaction.message.member.roles.has('724650792660893826') && (reaction.count >= 3)) {
+		reaction.message.pin();
+	}
+	
+	// The reaction is now also fully available and the properties will be reflected accurately:
+	//console.log(reaction.emoji.name + ' with reaction');
+});
+
+bot.login('Mjg4Mzk5MjY3NzEyOTkxMjMz.XvzeTQ.OwCCxFvHZKBKuqEzdi9iWB5LVvE');
